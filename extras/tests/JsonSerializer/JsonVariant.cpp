183c7234435af771630b1533b21480e27b651c77
Benoit Blanchon <git@benoitblanchon.fr>
2025-11-07 09:25:00 +0100

    JsonSerializer: refactor tests for simple values

--- extras/tests/JsonSerializer/JsonVariant.cpp
+++ extras/tests/JsonSerializer/JsonVariant.cpp
@@ -9,124 +9,78 @@
 #include "Literals.hpp"
 
 template <typename T>
-void check(T value, const std::string& expected) {
+std::string serialize(T value) {
   JsonDocument doc;
   doc.to<JsonVariant>().set(value);
-  char buffer[256] = "";
-  size_t returnValue = serializeJson(doc, buffer, sizeof(buffer));
-  REQUIRE(expected == buffer);
-  REQUIRE(expected.size() == returnValue);
+  std::string output;
+  serializeJson(doc, output);
+  return output;
 }
 
 TEST_CASE("serializeJson(JsonVariant)") {
-  SECTION("Undefined") {
-    check(JsonVariant(), "null");
-  }
-
-  SECTION("Null string") {
-    check(static_cast<char*>(0), "null");
+  SECTION("JsonVariant") {
+    CHECK(serialize(JsonVariant()) == "null");
   }
 
   SECTION("const char*") {
-    check("hello", "\"hello\"");
+    CHECK(serialize(static_cast<const char*>(0)) == "null");
+    CHECK(serialize("hello") == "\"hello\"");
   }
 
-  SECTION("string") {
-    check("hello"_s, "\"hello\"");
-
-    SECTION("Escape quotation mark") {
-      check("hello \"world\""_s, "\"hello \\\"world\\\"\"");
-    }
-
-    SECTION("Escape reverse solidus") {
-      check("hello\\world"_s, "\"hello\\\\world\"");
-    }
-
-    SECTION("Don't escape solidus") {
-      check("fifty/fifty"_s, "\"fifty/fifty\"");
-    }
-
-    SECTION("Don't escape single quote") {
-      check("hello'world"_s, "\"hello'world\"");
-    }
-
-    SECTION("Escape backspace") {
-      check("hello\bworld"_s, "\"hello\\bworld\"");
-    }
-
-    SECTION("Escape formfeed") {
-      check("hello\fworld"_s, "\"hello\\fworld\"");
-    }
-
-    SECTION("Escape linefeed") {
-      check("hello\nworld"_s, "\"hello\\nworld\"");
-    }
-
-    SECTION("Escape carriage return") {
-      check("hello\rworld"_s, "\"hello\\rworld\"");
-    }
-
-    SECTION("Escape tab") {
-      check("hello\tworld"_s, "\"hello\\tworld\"");
-    }
-
-    SECTION("NUL char") {
-      check("hello\0world"_s, "\"hello\\u0000world\"");
-    }
+  SECTION("std::string") {
+    CHECK(serialize("hello"_s) == "\"hello\"");
+    CHECK(serialize("hello \"world\""_s) == "\"hello \\\"world\\\"\"");
+    CHECK(serialize("hello\\world"_s) == "\"hello\\\\world\"");
+    CHECK(serialize("fifty/fifty"_s) == "\"fifty/fifty\"");
+    CHECK(serialize("hello'world"_s) == "\"hello'world\"");
+    CHECK(serialize("hello\bworld"_s) == "\"hello\\bworld\"");
+    CHECK(serialize("hello\fworld"_s) == "\"hello\\fworld\"");
+    CHECK(serialize("hello\nworld"_s) == "\"hello\\nworld\"");
+    CHECK(serialize("hello\rworld"_s) == "\"hello\\rworld\"");
+    CHECK(serialize("hello\tworld"_s) == "\"hello\\tworld\"");
+    CHECK(serialize("hello\0world"_s) == "\"hello\\u0000world\"");
   }
 
   SECTION("SerializedValue<const char*>") {
-    check(serialized("[1,2]"), "[1,2]");
+    CHECK(serialize(serialized("[1,2]")) == "[1,2]");
   }
 
   SECTION("SerializedValue<std::string>") {
-    check(serialized("[1,2]"_s), "[1,2]");
+    CHECK(serialize(serialized("[1,2]"_s)) == "[1,2]");
   }
 
-  SECTION("Double") {
-    check(3.1415927, "3.1415927");
+  SECTION("double") {
+    CHECK(serialize(3.1415927) == "3.1415927");
   }
 
-  SECTION("Float") {
+  SECTION("float") {
     REQUIRE(sizeof(float) == 4);
-    check(3.1415927f, "3.141593");
+    CHECK(serialize(3.1415927f) == "3.141593");
   }
 
-  SECTION("Zero") {
-    check(0, "0");
+  SECTION("int") {
+    CHECK(serialize(0) == "0");
+    CHECK(serialize(42) == "42");
+    CHECK(serialize(-42) == "-42");
   }
 
-  SECTION("Integer") {
-    check(42, "42");
+  SECTION("unsigned long") {
+    CHECK(serialize(4294967295UL) == "4294967295");
   }
 
-  SECTION("NegativeLong") {
-    check(-42, "-42");
-  }
-
-  SECTION("UnsignedLong") {
-    check(4294967295UL, "4294967295");
-  }
-
-  SECTION("True") {
-    check(true, "true");
-  }
-
-  SECTION("OneFalse") {
-    check(false, "false");
+  SECTION("bool") {
+    CHECK(serialize(true) == "true");
+    CHECK(serialize(false) == "false");
   }
 
 #if ARDUINOJSON_USE_LONG_LONG
-  SECTION("NegativeInt64") {
-    check(-9223372036854775807 - 1, "-9223372036854775808");
+  SECTION("int64_t") {
+    CHECK(serialize(-9223372036854775807 - 1) == "-9223372036854775808");
+    CHECK(serialize(9223372036854775807) == "9223372036854775807");
   }
 
-  SECTION("PositiveInt64") {
-    check(9223372036854775807, "9223372036854775807");
-  }
-
-  SECTION("UInt64") {
-    check(18446744073709551615U, "18446744073709551615");
+  SECTION("uint64_t") {
+    CHECK(serialize(18446744073709551615U) == "18446744073709551615");
   }
 #endif
 }
