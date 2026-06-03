bf148c0231a0b926a60c0f4939f5c84f5a7ff330
Maxim Prokhorov <prokhorov.max@outlook.com>
2026-06-03 17:58:16 +0300

    New type-traits checks and change stdlib IsBaseOf for something local

--- test/Misc/TypeTraits.cpp
+++ test/Misc/TypeTraits.cpp
@@ -20,15 +20,36 @@ using ArduinoJson::Internals::IsVariant;
 using ArduinoJson::Internals::JsonObjectSubscript;
 using ArduinoJson::Internals::JsonVariantBase;
 
+struct BaseOne {
+};
+
+struct DerivedFromBaseOne : public BaseOne {
+};
+
+struct DerivedFromDerived : public DerivedFromBaseOne {
+};
+
+struct BaseTwo {
+};
+
+
+struct DerivedFromBaseTwo : public BaseTwo {
+};
+
 TEST_CASE("TypeTraits") {
   SECTION("IsBaseOf") {
-    REQUIRE_FALSE(
-        static_cast<bool>(IsBaseOf<std::istream, std::ostringstream>::value));
-    REQUIRE(
-        static_cast<bool>(IsBaseOf<std::istream, std::istringstream>::value));
-    REQUIRE(static_cast<bool>(
-        IsBaseOf<JsonVariantBase<JsonObjectSubscript<const char*> >,
-                 JsonObjectSubscript<const char*> >::value));
+    REQUIRE_FALSE(IsBaseOf<BaseTwo, BaseOne>::value);
+    REQUIRE_FALSE(IsBaseOf<BaseOne, BaseTwo>::value);
+    REQUIRE_FALSE(IsBaseOf<BaseOne, DerivedFromBaseTwo>::value);
+    REQUIRE(IsBaseOf<BaseOne, DerivedFromBaseOne>::value);
+    REQUIRE(IsBaseOf<BaseOne, DerivedFromDerived>::value);
+    REQUIRE(IsBaseOf<BaseTwo, DerivedFromBaseTwo>::value);
+    REQUIRE(IsBaseOf<
+      JsonVariantBase<JsonObjectSubscript<const char*> >,
+      JsonObjectSubscript<const char*> >::value);
+    REQUIRE(IsBaseOf<
+      JsonVariantBase<JsonObjectSubscript<std::string> >,
+      JsonObjectSubscript<std::string> >::value);
   }
 
   SECTION("IsPointer") {
@@ -42,6 +63,8 @@ TEST_CASE("TypeTraits") {
   }
 
   SECTION("IsArray") {
+    REQUIRE_FALSE((IsArray<JsonObjectSubscript<const char[10]> >::value));
+    REQUIRE_FALSE((IsArray<std::string>::value));
     REQUIRE_FALSE((IsArray<const char*>::value));
     REQUIRE_FALSE((IsArray<const char*&>::value));
     REQUIRE_FALSE((IsArray<const char(&)[10]>::value));
@@ -50,13 +73,15 @@ TEST_CASE("TypeTraits") {
   }
 
   SECTION("IsVariant") {
-    REQUIRE(
-        static_cast<bool>(IsVariant<JsonObjectSubscript<const char*> >::value));
-    REQUIRE(static_cast<bool>(IsVariant<JsonVariant>::value));
+    REQUIRE_FALSE(IsVariant<std::string>::value);
+    REQUIRE(IsVariant<JsonObjectSubscript<const char*> >::value);
+    REQUIRE(IsVariant<JsonObjectSubscript<std::string> >::value);
+    REQUIRE(IsVariant<JsonVariant>::value);
   }
 
   SECTION("IsConst") {
     REQUIRE_FALSE((IsConst<char>::value));
+    REQUIRE((IsConst<const std::string>::value));
     REQUIRE((IsConst<const char>::value));
   }
 }
