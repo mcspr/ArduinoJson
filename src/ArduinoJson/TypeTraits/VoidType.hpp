fac2b9e5107b8b8b3cba1b5f059ad1efeffbc9be
Maxim Prokhorov <prokhorov.max@outlook.com>
2026-07-24 21:14:41 +0300

    Clarify std::void_t workaround

--- src/ArduinoJson/TypeTraits/VoidType.hpp
+++ src/ArduinoJson/TypeTraits/VoidType.hpp
@@ -6,6 +6,8 @@ namespace ArduinoJson {
 namespace Internals {
 namespace TypeTraits {
 
+// https://gcc.gnu.org/bugzilla/show_bug.cgi?id=64395
+// workaround for gcc4.8 & gcc4.9 broken void_t as template param
 template <typename ...>
 struct MakeVoidType {
   typedef void type;
