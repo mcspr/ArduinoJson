cb52c2a0863fa5c75346f0006c977874dc49dda9
Maxim Prokhorov <prokhorov.max@outlook.com>
2026-08-11 03:55:15 +0300

    IsFloatingPoint & IsIntegral replace IsSame with matching through specializations
    Fixup existing const / volatile / const volatile matching as well
    Drop redundant checks for identical types
    Fixup previously mistyped MSVC int64

--- src/ArduinoJson/TypeTraits/IsFloatingPoint.hpp
+++ src/ArduinoJson/TypeTraits/IsFloatingPoint.hpp
@@ -4,17 +4,45 @@
 
 #pragma once
 
-#include "IsSame.hpp"
-#include "Or.hpp"
+#include "Constant.hpp"
 
 namespace ArduinoJson {
 namespace Internals {
+namespace TypeTraits {
+
+template <typename>
+struct IsFloatingPointType : FalseType {
+};
+
+template <>
+struct IsFloatingPointType<float> : TrueType {
+};
+
+template <>
+struct IsFloatingPointType<double> : TrueType {
+};
+
+template <typename T>
+struct IsFloatingPointImpl : IsFloatingPointType<T>::type {
+};
+
+template <typename T>
+struct IsFloatingPointImpl<T const> : IsFloatingPointType<T>::type {
+};
+
+template <typename T>
+struct IsFloatingPointImpl<T volatile> : IsFloatingPointType<T>::type {
+};
+
+template <typename T>
+struct IsFloatingPointImpl<T const volatile> : IsFloatingPointType<T>::type {
+};
+
+}
 
 // A meta-function that returns true if T is a floating point type
 template <typename T>
-struct IsFloatingPoint : Or<
-    IsSame<T, float>,
-    IsSame<T, double>>::type {
+struct IsFloatingPoint : TypeTraits::IsFloatingPointImpl<T>::type {
 };
 
 }  // namespace Internals
--- src/ArduinoJson/TypeTraits/IsIntegral.hpp
+++ src/ArduinoJson/TypeTraits/IsIntegral.hpp
@@ -7,67 +7,153 @@
 #include "../Configuration.hpp"
 
 #include "Constant.hpp"
-#include "IsSame.hpp"
 #include "Or.hpp"
 
 namespace ArduinoJson {
 namespace Internals {
-namespace TypeTraitsImpl {
+namespace TypeTraits {
+
+template <typename>
+struct IsSignedTypeImpl : FalseType {
+};
+
+template <>
+struct IsSignedTypeImpl<signed char> : TrueType {
+};
+
+template <>
+struct IsSignedTypeImpl<signed short> : TrueType {
+};
+
+template <>
+struct IsSignedTypeImpl<signed int> : TrueType {
+};
+
+template <>
+struct IsSignedTypeImpl<signed long> : TrueType {
+};
+
+#if ARDUINOJSON_USE_LONG_LONG
+template <>
+struct IsSignedTypeImpl<signed long long> : TrueType {
+};
+#endif
+
+#if ARDUINOJSON_USE_INT64
+template <>
+struct IsSignedTypeImpl<signed __int64> : TrueType {
+};
+#endif
 
 // A meta-function that returns true if T is a type that is signed.
 template <typename T>
-struct IsSignedType :
-      Or<IsSame<T, signed>,
-         IsSame<T, signed char>,
-         IsSame<T, signed short>,
-         IsSame<T, signed int>,
-         IsSame<T, signed long>,
-         IsSame<T, signed long int>,
-#if ARDUINOJSON_USE_LONG_LONG
-        IsSame<T, signed long long>,
-        IsSame<T, signed long long int>,
-#endif
-#if ARDUINOJSON_USE_INT64
-        IsSame<T, unsigned __int64>,
-#endif
-        FalseType>::type {
+struct IsSignedType : IsSignedTypeImpl<T>::type {
 };
+
+template <typename T>
+struct IsSignedType<T const> : IsSignedTypeImpl<T>::type {
+};
+
+template <typename T>
+struct IsSignedType<T volatile> : IsSignedTypeImpl<T>::type {
+};
+
+template <typename T>
+struct IsSignedType<T const volatile> : IsSignedTypeImpl<T>::type {
+};
+
+template <typename>
+struct IsUnsignedTypeImpl : FalseType {
+};
+
+template <>
+struct IsUnsignedTypeImpl<unsigned char> : TrueType {
+};
+
+template <>
+struct IsUnsignedTypeImpl<unsigned short> : TrueType {
+};
+
+template <>
+struct IsUnsignedTypeImpl<unsigned int> : TrueType {
+};
+
+template <>
+struct IsUnsignedTypeImpl<unsigned long> : TrueType {
+};
+
+#if ARDUINOJSON_USE_LONG_LONG
+template <>
+struct IsUnsignedTypeImpl<unsigned long long> : TrueType {
+};
+#endif
+
+#if ARDUINOJSON_USE_INT64
+template <>
+struct IsUnsignedTypeImpl<unsigned __int64> : TrueType {
+};
+#endif
 
 // A meta-function that returns true if T is a type that is unsigned.
 template <typename T>
-struct IsUnsignedType :
-      Or<IsSame<T, unsigned char>,
-         IsSame<T, unsigned short>,
-         IsSame<T, unsigned int>,
-         IsSame<T, unsigned long>,
-         IsSame<T, unsigned long int>,
-#if ARDUINOJSON_USE_LONG_LONG
-         IsSame<T, unsigned long long>,
-         IsSame<T, unsigned long long int>,
-#endif
-#if ARDUINOJSON_USE_INT64
-         IsSame<T, unsigned __int64>,
-#endif
-        FalseType>::type {
+struct IsUnsignedType : IsUnsignedTypeImpl<T>::type {
 };
 
 template <typename T>
-struct IsUnsignedType<const T> : IsUnsignedType<T>::type {
+struct IsUnsignedType<T const> : IsUnsignedType<T>::type {
+};
+
+template <typename T>
+struct IsUnsignedType<T volatile> : IsUnsignedType<T>::type {
+};
+
+template <typename T>
+struct IsUnsignedType<T const volatile> : IsUnsignedType<T>::type {
+};
+
+// A meta-function that matches integral types that are neither explicitly signed or unsigned
+template <typename>
+struct IsIntegralType : FalseType {
+};
+
+template <>
+struct IsIntegralType<char> : TrueType {
+};
+
+template <>
+struct IsIntegralType<bool> : TrueType {
+};
+
+// A meta-function that combines all of the type checkers above
+template <typename T>
+struct IsIntegralTypeImpl :
+    Or<IsSignedType<T>,
+       IsUnsignedType<T>,
+       IsIntegralType<T>>::type {
+
+};
+
+template <typename T>
+struct IsIntegralImpl : IsIntegralTypeImpl<T>::type {
+};
+
+template <typename T>
+struct IsIntegralImpl<T const > : IsIntegralTypeImpl<T>::type {
+};
+
+template <typename T>
+struct IsIntegralImpl<T volatile> : IsIntegralTypeImpl<T>::type {
+};
+
+template <typename T>
+struct IsIntegralImpl<T const volatile> : IsIntegralTypeImpl<T>::type {
 };
 
 }
 
 // A meta-function that returns true if T is an integral type.
 template <typename T>
-struct IsIntegral :
-    Or<TypeTraitsImpl::IsSignedType<T>,
-       TypeTraitsImpl::IsUnsignedType<T>,
-       IsSame<T, char>,
-       IsSame<T, bool>>::type {
-};
-
-template <typename T>
-struct IsIntegral<const T> : IsIntegral<T> {
+struct IsIntegral : TypeTraits::IsIntegralImpl<T>::type {
 };
 
 }  // namespace Internals
