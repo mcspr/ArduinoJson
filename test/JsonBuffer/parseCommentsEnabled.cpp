c55e7b0680607d3f9fc1fbfd16fcb4ec7fc4f874
Maxim Prokhorov <prokhorov.max@outlook.com>
2026-09-03 16:33:42 +0300

    Adapt some test cases from jsonc spec
    Apparently, various non-json features here fit very well into its implementation guidelines
    ref. https://jsonc.org/#single-line-comments
    ref. https://jsonc.org/#multi-line-comments

--- test/JsonBuffer/parseCommentsEnabled.cpp
+++ test/JsonBuffer/parseCommentsEnabled.cpp
@@ -53,6 +53,66 @@ TEST_CASE("Encountering comments when comments are enabled") {
       REQUIRE_FALSE(jb.parse(testCase).success());
     }
   }
+
+  SECTION("Single-line comments") {
+    // ref. https://jsonc.org/#single-line-comments
+    std::string testCase = R"(
+{
+    // This is a single-line comment
+    "name": "John Doe",
+    "age": 30 // This is another single line comment
+}
+    )";
+
+    JsonVariant variant = jb.parse(testCase);
+    REQUIRE(variant.success());
+    REQUIRE(variant.is<JsonObject>());
+
+    JsonObject& obj = variant.as<JsonObject&>();
+    REQUIRE(2 == obj.size());
+    REQUIRE(obj["name"] == "John Doe");
+    REQUIRE(obj["age"] == 30);
+  }
+
+  SECTION("Multi-line comments") {
+    // ref. https://jsonc.org/#multi-line-comments
+    std::string testCase = R"(
+{
+    /*
+      This is a block comment
+      that spans multiple lines
+    */
+    "name": "Jane Doe",
+    "age": /* This is a single-line block comment */ 25
+})";
+
+    JsonVariant variant = jb.parse(testCase);
+    REQUIRE(variant.success());
+    REQUIRE(variant.is<JsonObject>());
+
+    JsonObject& obj = variant.as<JsonObject&>();
+    REQUIRE(2 == obj.size());
+    REQUIRE(obj["name"] == "Jane Doe");
+    REQUIRE(obj["age"] == 25);
+  }
+
+  SECTION("Nested multi-line comments") {
+    // ref. https://jsonc.org/#multi-line-comments
+    std::string testCase = R"(
+{
+/* OUTER start
+  /* NESTED block comments are not supported.
+      OUTER block comment will end here --> */
+
+  OUTER end
+*/
+    "name": "John Doe",
+    "age": 30
+}
+    )";
+
+    REQUIRE_FALSE(jb.parse(testCase).success());
+  }
 }
 
 
