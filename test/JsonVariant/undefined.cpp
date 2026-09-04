c4b6dd0cf2fa118b91a2c75a387ba9217885ebef
Maxim Prokhorov <prokhorov.max@outlook.com>
2026-09-04 16:51:42 +0300

    JsonVariant undefined tests updated for all included types

--- test/JsonVariant/undefined.cpp
+++ test/JsonVariant/undefined.cpp
@@ -8,24 +8,70 @@
 TEST_CASE("JsonVariant undefined") {
   JsonVariant variant;
 
+  SECTION("AsCharReturns0") {
+    REQUIRE(char{0} == variant.as<char>());
+  }
+
+  SECTION("AsShortReturns0") {
+    REQUIRE(short{0} == variant.as<short>());
+  }
+
+  SECTION("AsIntReturns0") {
+    REQUIRE(int{0} == variant.as<int>());
+  }
+
   SECTION("AsLongReturns0") {
-    REQUIRE(0 == variant.as<long>());
+    REQUIRE(0L == variant.as<long>());
   }
 
-  SECTION("AsUnsignedReturns0") {
-    REQUIRE(0 == variant.as<unsigned>());
+#if ARDUINOJSON_USE_LONG_LONG
+  SECTION("AsLongLongReturns0") {
+    REQUIRE(0LL == variant.as<long long>());
+  }
+#endif
+
+  SECTION("AsUnsignedCharReturns0") {
+    REQUIRE(0 == variant.as<unsigned char>());
   }
 
-  SECTION("AsStringReturnsNull") {
-    REQUIRE(nullptr == variant.as<const char*>());
+  SECTION("AsUnsignedShortReturns0") {
+    REQUIRE(0 == variant.as<unsigned short>());
   }
 
+  SECTION("AsUnsignedIntReturns0") {
+    REQUIRE(0 == variant.as<unsigned int>());
+  }
+
+  SECTION("AsUnsignedLongReturns0") {
+    REQUIRE(0UL == variant.as<unsigned long>());
+  }
+
+#if ARDUINOJSON_USE_LONG_LONG
+  SECTION("AsUnsignedLongLongReturns0") {
+    REQUIRE(0ULL == variant.as<unsigned long long>());
+  }
+#endif
+
+  SECTION("AsFloatReturns0") {
+    REQUIRE(0.0 == variant.as<float>());
+  }
+
+#if ARDUINOJSON_USE_DOUBLE
   SECTION("AsDoubleReturns0") {
-    REQUIRE(0 == variant.as<double>());
+    REQUIRE(0.0 == variant.as<double>());
   }
+#endif
 
   SECTION("AsBoolReturnsFalse") {
     REQUIRE(false == variant.as<bool>());
+  }
+
+  SECTION("AsCstringReturnsNullptr") {
+    REQUIRE(nullptr == variant.as<const char*>());
+  }
+
+  SECTION("AsStdStringReturnsNullString") {
+    REQUIRE(std::string("null") == variant.as<std::string>());
   }
 
   SECTION("AsArrayReturnInvalid") {
